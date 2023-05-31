cb66757ef20864e96eca9c7f6f09aa0327cd1d60
Opdata <pb03641@gmail.com>
2023-05-31 15:34:26 +0900

    test: dummy model 추가 및 멀티 렌더링 코드 테스트

--- src/App.js
+++ src/App.js
@@ -1,134 +1,55 @@
-// import React, { Suspense } from 'react';
-// import { Canvas } from '@react-three/fiber';
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-// import { useLoader } from '@react-three/fiber';
-// import { OrbitControls, Html, Loader } from '@react-three/drei';
-
-// function Model({ url }) {
-//   const gltf = useLoader(GLTFLoader, url);
-//   console.log('gltf : ', gltf);
-//   return <primitive object={gltf.scene} />;
-// }
-
-// export default function App() {
-//   return (
-//     <Canvas style={{ height: '100vh', width: '100vw' }}>
-//       <ambientLight intensity={0.5} />
-//       <Suspense
-//         fallback={
-//           <Html center>
-//             <Loader />
-//           </Html>
-//         }
-//       >
-//         <Model url="/stelsi.glb" />
-//       </Suspense>
-//       <OrbitControls />
-//     </Canvas>
-//   );
-// }
-
-// import React from 'react';
-// import { Canvas } from '@react-three/fiber';
-// import { useGLTF } from '@react-three/drei';
-// import { OrbitControls } from '@react-three/drei';
-
-// const MyGLTFComponent = () => {
-//   const { nodes } = useGLTF('/house.glb');
-
-//   return (
-//     <>
-//       {Object.values(nodes).map((node, i) => (
-//         <mesh
-//           castShadow
-//           receiveShadow
-//           key={i}
-//           geometry={node.geometry}
-//           material={node.material}
-//           position={node.position}
-//         />
-//       ))}
-//     </>
-//   );
-// };
-
-// const App = () => {
-//   return (
-//     <Canvas
-//       style={{ height: '800px' }}
-//       camera={{ position: [0, 10, 10], fov: 100 }}
-//       shadows
-//     >
-//       <ambientLight />
-//       <directionalLight castShadow position={[2.5, 2.5, 2.5]} intensity={0.7} />
-//       <pointLight position={[10, 10, 10]} />
-//       <MyGLTFComponent />
-//       <OrbitControls makeDefault />
-//     </Canvas>
-//   );
-// };
-
-// export default App;
-
-// FIXME: 1000개 glb 렌더링 코드 및 카메라 최소 줌 인, 줌 아웃 설정, 모델 스케일 축소 적용
-import { useRef } from 'react';
+// FIXME: 멀티 모델 데이터 렌더링 기초 코드
+import { Vector3, MeshStandardMaterial } from 'three';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { useGLTF } from '@react-three/drei';
-import { OrbitControls } from '@react-three/drei';
+import { useGLTF, OrbitControls } from '@react-three/drei';
 
-const MyGLTFComponent = () => {
-  const { nodes } = useGLTF('/house_draco.glb');
-  const modelWidth = 5; // adjust this value based on the actual width of your models
+import modelList from './modelList';
 
-  const cameraRef = useRef();
-
-  // Check and adjust camera position every frame
-  useFrame(({ camera }) => {
-    console.log('x : ', camera.position.x);
-    console.log('y : ', camera.position.y);
-    console.log('z : ', camera.position.z);
-    camera.position.x = Math.max(Math.min(camera.position.x, 10), -10); // Limit x between -10 and 10
-    camera.position.y = Math.max(Math.min(camera.position.y, 10), -10); // Limit y between -10 and 10
-    camera.position.z = Math.max(Math.min(camera.position.z, 10), -10); // Limit z between -10 and 10
-    camera.updateProjectionMatrix(); // Need to update after changing position
-  });
+const MyGLTFComponent = ({ id, url, location }) => {
+  const { nodes } = useGLTF(url);
 
   return (
-    <perspectiveCamera ref={cameraRef}>
-      {Array.from({ length: 1000 }).map((_, i) =>
-        Object.values(nodes).map((node, j) => (
+    <>
+      {Object.values(nodes).map((node, j) => {
+        return (
           <mesh
             // castShadow
             // receiveShadow
-            key={`${i}-${j}`}
+            key={`${id}-${j}`}
             geometry={node.geometry}
             material={node.material}
+            //   position={position}
             position={[
-              node.position.x + i * modelWidth,
-              node.position.y,
-              node.position.z,
+              node.position.x + location.x,
+              node.position.y + location.y,
+              node.position.z + location.z,
             ]}
             scale={[0.5, 0.5, 0.5]} // 50% reduction in size
           />
-        ))
-      )}
-    </perspectiveCamera>
+        );
+      })}
+    </>
   );
 };
 
 const App = () => {
   return (
-    <Canvas
-      style={{ height: '800px' }}
-      camera={{ position: [0, 10, 10], fov: 100 }}
-      //   shadows
-    >
-      <ambientLight />
-      <directionalLight castShadow position={[2.5, 2.5, 2.5]} intensity={0.7} />
-      <pointLight position={[10, 10, 10]} />
-      <MyGLTFComponent />
-      <OrbitControls minDistance={5} maxDistance={10} />
-    </Canvas>
+    <>
+      <Canvas
+        style={{ height: '800px' }}
+        camera={{ position: [15, 15, 15], fov: 120 }}
+        // shadows
+      >
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        {modelList.map(({ id, url, location }) => {
+          return (
+            <MyGLTFComponent key={id} id={id} url={url} location={location} />
+          );
+        })}
+        <OrbitControls makeDefault />
+      </Canvas>
+    </>
   );
 };
 
---
+++ src/modelList.js
@@ -0,0 +1,102 @@
+export default [
+  {
+    id: 300046,
+    name: 'C1A 300046',
+    url: '/house_draco.glb',
+    location: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+  },
+  {
+    id: 300050,
+    name: 'C1A 300050',
+    url: '/house_draco.glb',
+    location: {
+      x: 5,
+      y: 0,
+      z: 0,
+    },
+  },
+  {
+    id: 300054,
+    name: 'C1A 300054',
+    url: '/house_draco.glb',
+    location: {
+      x: 10,
+      y: 0,
+      z: 0,
+    },
+  },
+  {
+    id: 300058,
+    name: 'C1A 300058',
+    url: '/house_draco.glb',
+    location: {
+      x: 0,
+      y: 0,
+      z: 5,
+    },
+  },
+  {
+    id: 300062,
+    name: 'C1A 300046',
+    url: '/house_draco.glb',
+    location: {
+      x: 5,
+      y: 0,
+      z: 5,
+    },
+  },
+  {
+    id: 300066,
+    name: 'C1A 300062',
+    url: '/house_draco.glb',
+    location: {
+      x: 10,
+      y: 0,
+      z: 5,
+    },
+  },
+  {
+    id: 300072,
+    name: 'C1A 300066',
+    url: '/house_draco.glb',
+    location: {
+      x: 0,
+      y: 0,
+      z: 10,
+    },
+  },
+  {
+    id: 300070,
+    name: 'C1A 300070',
+    url: '/house_draco.glb',
+    location: {
+      x: 5,
+      y: 0,
+      z: 10,
+    },
+  },
+  {
+    id: 300074,
+    name: 'C1A 300074',
+    url: '/house_draco.glb',
+    location: {
+      x: 10,
+      y: 0,
+      z: 10,
+    },
+  },
+  {
+    id: 300076,
+    name: 'C1A 300078',
+    url: '/house_draco.glb',
+    location: {
+      x: 15,
+      y: 0,
+      z: 10,
+    },
+  },
+];
